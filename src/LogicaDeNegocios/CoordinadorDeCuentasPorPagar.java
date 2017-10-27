e7d830e987aa521989d08589748b1d842989474b
Laura <Laura@10.234.200.74>
2017-10-27 00:00:08 -0600

    Creacion de clase

---
+++ src/LogicaDeNegocios/CoordinadorDeCuentasPorPagar.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LogicaDeNegocios;
+
+import AccesoDatos.GestorDeCuentasPorPagar;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Laura
+ */
+public class CoordinadorDeCuentasPorPagar {
+
+    public CoordinadorDeCuentasPorPagar() {
+    }
+    public ResultSet ListarCuentasPorPagar() throws ClassNotFoundException, SQLException{
+         GestorDeCuentasPorPagar elGestor= new GestorDeCuentasPorPagar();
+         return elGestor.ListarCuentasPorPagar();
+    }
+}
